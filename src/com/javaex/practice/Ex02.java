6d7b501ac590ced028b7865dd1ccc7953a64d2e7
최정필 <wjdvlf1407@gmail.com>
2022-04-20 16:53:42 +0900

    Ex02 출력 후

--- src/com/javaex/practice/Ex02.java
+++ src/com/javaex/practice/Ex02.java
@@ -4,7 +4,7 @@ public class Ex02 {
 
 	public static void main(String[] args) {
 
-		double[] nums = new double[3];	//	주소 개수
+		double[] nums = new double[3];	// 주소 개수
 
 		nums[0] = 6.7;
 		nums[1] = 3.3;
@@ -13,6 +13,13 @@ public class Ex02 {
 		for (int i = 0; i < nums.length; i++) {
 			System.out.println(nums[i]);
 		}
+		/*	
+		출력 결과:
+		
+		6.7
+		3.3
+		1.2
+		*/
 	}
 
 }
