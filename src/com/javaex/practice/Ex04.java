520454314beef35080da1f7784d881330b3f6ed8
최정필 <wjdvlf1407@gmail.com>
2022-04-20 21:59:00 +0900

    Ex08 오류 수정중

--- src/com/javaex/practice/Ex04.java
+++ src/com/javaex/practice/Ex04.java
@@ -23,4 +23,3 @@ public class Ex04 {
 	}
 
 }
-
--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -4,22 +4,31 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 
-		int[] num = new int[7];
+		int[] num = new int[6];
 
-		num[0] = (int) (Math.random() * 45) + 1;
-		num[1] = (int) (Math.random() * 45) + 1;
-		num[2] = (int) (Math.random() * 45) + 1;
-		num[3] = (int) (Math.random() * 45) + 1;
-		num[4] = (int) (Math.random() * 45) + 1;
-		num[5] = (int) (Math.random() * 45) + 1;
+//		num[0] = (int) (Math.random() * 45) + 1;
+//		num[1] = (int) (Math.random() * 45) + 1;
+//		num[2] = (int) (Math.random() * 45) + 1;
+//		num[3] = (int) (Math.random() * 45) + 1;
+//		num[4] = (int) (Math.random() * 45) + 1;
+//		num[5] = (int) (Math.random() * 45) + 1;
+		
+		num[0] = 0;
+		num[1] = 1;
+		num[2] = 2;
+		num[3] = 3;
+		num[4] = 4;
+		num[5] = 5;
 
-		for (int i = 0; i < num.length - 1; i++) {
+		for (int i = 0; i < num.length; i++) {
 
 			for (int j = 1; j <= 45; j++) {
-				if (num[i] != num[i + 1] && num[i] == j && i < num.length) {
+				if (num[i] != num[i + 1] && num[i] == j && i < num.length) {		//	num[i+1]에서 오
 					num[i] = j;
-				} else {
+					System.out.println("skdjflskdjfslkdjflksjflsfsf");
+				}else{	
 					
+					System.out.println("나머지");
 				}
 
 			}
