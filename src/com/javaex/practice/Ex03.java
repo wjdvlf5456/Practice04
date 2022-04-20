a9a2d864f5bb8c8ba157c17244c30db944a9a014
최정필 <wjdvlf1407@gmail.com>
2022-04-20 16:57:52 +0900

    Ex03 출력 전 예상

---
+++ src/com/javaex/practice/Ex03.java
@@ -0,0 +1,29 @@
+package com.javaex.practice;
+
+public class Ex03 {
+	
+	public static void main(String[] args) {
+		
+		int[] intA = {3, 6, 9};		//	intA의 주소는 하나이고 배열수는 3개이다.
+		
+		int[] intB;		//	intB는 intA와 다른 주소값을 가진다.
+		intB = intA;	
+		intB[0] = 20;
+		intB[2] = 10;
+		
+		for (int i = 0; i < intA.length; i++) {
+			System.out.println(intA[i]);
+		}
+		
+		/*
+		예상
+		
+		inB의 배열값은 intA에 영향을 주지 않으므로 
+		3
+		6
+		9
+		가 출력 될 것이다.
+		 */
+	}
+
+}
