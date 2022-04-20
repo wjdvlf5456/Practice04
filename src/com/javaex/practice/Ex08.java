98d89b35b76b3143836f31f3cb780cc03f623818
최정필 <wjdvlf1407@gmail.com>
2022-04-21 00:40:21 +0900

    Ex08 디버깅중

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -4,38 +4,46 @@ public class Ex08 {
 
 	public static void main(String[] args) {
 
-		int[] num = new int[6];
-
-//		num[0] = (int) (Math.random() * 45) + 1;
-//		num[1] = (int) (Math.random() * 45) + 1;
-//		num[2] = (int) (Math.random() * 45) + 1;
-//		num[3] = (int) (Math.random() * 45) + 1;
-//		num[4] = (int) (Math.random() * 45) + 1;
-//		num[5] = (int) (Math.random() * 45) + 1;
 		
-		num[0] = 0;
-		num[1] = 1;
-		num[2] = 2;
-		num[3] = 3;
-		num[4] = 4;
-		num[5] = 5;
-
+		int[] num = new int[6];
+		
+		 
+		
+//		num[0] = 10;
+//		num[1] = 20;
+//		num[2] = 30;
+//		num[3] = 40;
+//		num[4] = 50;
+//		num[5] = 60;
+		
+		
+		
+		
 		for (int i = 0; i < num.length; i++) {
-
-			for (int j = 1; j <= 45; j++) {
-				if (num[i] != num[i + 1] && num[i] == j && i < num.length) {		//	num[i+1]에서 오류
-					num[i] = j;
-					System.out.println("skdjflskdjfslkdjflksjflsfsf");
-				}else{	
+			
+			num[i] = (int) (Math.random() * 45) + 1;
+			for (int j = i+1; j < num.length; j++) {
+				if (num[i]==num[j]) {
+					System.out.println(i+"=i값 같을때");
+					System.out.println(j+"=j값 같을 때");
+					continue;
+				}else if(num[i]!=num[j]){
+					System.out.println(i+"=i값 다를때");
+					System.out.println(j+"=j값 다를때");
+					break;
 					
-					System.out.println("나머지");
 				}
-
+				
 			}
-			System.out.print(num[i] + "\t");
-
+			System.out.print(num[i]+"\t");
+			//System.out.println(num[i]+"=num[i]값");
+			//System.out.println("");
+			
+		
 		}
-
+		System.out.println("sdfsdfs");
 	}
+
+
 
 }
