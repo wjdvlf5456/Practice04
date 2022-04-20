e7de0a75e44c36a9fa36610eb036b7a6c54b48c8
최정필 <wjdvlf1407@gmail.com>
2022-04-20 17:00:11 +0900

    Ex03 오류수정

--- src/com/javaex/practice/Ex03.java
+++ src/com/javaex/practice/Ex03.java
@@ -6,7 +6,7 @@ public class Ex03 {
 		
 		int[] intA = {3, 6, 9};		//	intA의 주소는 하나이고 배열수는 3개이다.
 		
-		int[] intB;		//	intB는 intA와 다른 주소값을 가진다.
+		int[] intB;		//	intB는 intA와 같은 주소값을 가진다.
 		intB = intA;	
 		intB[0] = 20;
 		intB[2] = 10;
@@ -23,6 +23,16 @@ public class Ex03 {
 		6
 		9
 		가 출력 될 것이다.
+		 */
+		
+		/*
+		정답
+		20
+		6
+		10
+		
+		new int[] 를 새로 만든 게 아니라 같은 주소를 가졌다.
+		
 		 */
 	}
 
