f686b7cd304f0cbd59b6d94404cc1ed48063ab82
최정필 <wjdvlf1407@gmail.com>
2022-04-20 21:50:22 +0900

    Ex04 수정

--- src/com/javaex/practice/Ex04.java
+++ src/com/javaex/practice/Ex04.java
@@ -7,28 +7,20 @@ public class Ex04 {
 		int[] data = { 1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31 };	// 배열은 13개
 
 		int sum = 0;
+		int count = 0;
 
 		for (int i = 0; i < data.length; i++) {
 			if (data[i] % 3 == 0) {
 				sum = sum + data[i];
+				count += 1;
 
 			}
 
 		}
-		System.out.println();
+		System.out.println("주어진 배열에서 3의 배수의 개수=>" + count);
 		System.out.println("주어진 배열에서 3의 배수의 합=>" + sum);
 
 	}
 
 }
 
-/*		int sum = 0;
-				for (int i = 0; i < data.length; i++) {
-		
-					if (data[i] % 3 == 0) {
-						sum = sum + data[i];
-						System.out.println("주어진 배열에서 3의 배수의 개수=>" + data[i] / 3);
-		
-					}
-		
-				}*/
