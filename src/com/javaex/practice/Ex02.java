c05de0902c50dd86c586059b6803ca646d336e7c
최정필 <wjdvlf1407@gmail.com>
2022-04-20 16:52:54 +0900

    Ex02 출력 전

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,18 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+
+		double[] nums = new double[3];	//	주소 개수
+
+		nums[0] = 6.7;
+		nums[1] = 3.3;
+		nums[2] = 1.2;
+
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+	}
+
+}
