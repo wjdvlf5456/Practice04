c2b3946963290e6204c1659a4e3ef1fc41064abf
최정필 <wjdvlf1407@gmail.com>
2022-04-20 18:10:10 +0900

    Ex06 수정중

--- src/com/javaex/practice/Ex06.java
+++ src/com/javaex/practice/Ex06.java
@@ -4,7 +4,13 @@ public class Ex06 {
 	
 	public static void main(String[] args) {
 		
-		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};
+		char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l'};		//	16개
+		
+		for (int i = 0; i < c.length; i++) {
+			System.out.println(c[i]);
+		}
+		
+		
 		
 	}
 
