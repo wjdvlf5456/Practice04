28ae8d87c21692e9cfd852a88f7717eec6046063
최정필 <wjdvlf1407@gmail.com>
2022-04-21 00:53:32 +0900

    Ex08 디버깅4

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -11,22 +11,17 @@ public class Ex08 {
 			num[i] = (int) (Math.random() * 45) + 1;
 			for (int j = i + 1; j < num.length; j++) {
 				if (num[i] == num[j]) {
-					System.out.println(i + "=i값 같을때");
-					System.out.println(j + "=j값 같을 때");
 					continue;
 				} else if (num[i] != num[j]) {
-					System.out.println(i + "=i값 다를때");
-					System.out.println(j + "=j값 다를때");
-					System.out.println(num[i] + "\t");		// 마지막 값이 안 나오는 것을 해결해야함
+					System.out.print(num[i] + "\t");		// 마지막 값이 안 나오는 것을 해결해야함
 					break;
 
 				}
+				//else if	end
 
-			}
-			// System.out.println(num[i]+"=num[i]값");
-			// System.out.println("");
+			}//2nd for end
 
-		}
+		}//1st for end
 	}
 
 }
