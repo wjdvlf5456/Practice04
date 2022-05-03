cfc37216bcd3219e81c7532f16ee26f1126ee351
최정필 <wjdvlf1407@gmail.com>
2022-05-03 17:02:47 +0900

    Ex08

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -7,21 +7,18 @@ public class Ex08 {
 		int[] num = new int[6];
 
 		for (int i = 0; i < num.length; i++) {
-
 			num[i] = (int) (Math.random() * 45) + 1;
-			for (int j = i + 1; j < num.length; j++) {
+
+			for (int j = 0; j < i; j++) {
 				if (num[i] == num[j]) {
-					continue;
-				} else if (num[i] != num[j]) {
-					System.out.print(num[i] + "\t");		// 마지막 값이 안 나오는 것을 해결해야함
-					break;
-
+					--i;
+					break;					//중복체크
 				}
-				//else if	end
 
-			}//2nd for end
+			}
 
-		}//1st for end
+			System.out.print(num[i] + "\t");
+		}// 1st for end
 	}
 
 }
